9f04710c90374be8e852724aca56c387cb83b85b
p-stern <88513114+p-stern@users.noreply.github.com>
2023-04-27 00:15:29 -0700

    Delete Unit18 directory

--- Unit18/python-syntax/words.py
+++
@@ -1,7 +0,0 @@
-def print_upper_words(words, must_start_with):
-    """print one word per line in uppercase, must start with one of the must_start_with parameters"""
-
-    for word in words:
-        for letter in must_start_with:
-            if word[0].upper() == letter.upper():
-                print(word.upper())
